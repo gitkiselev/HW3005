4cde52ff91f1eb4828043d8d9fe6fc6ba91e5318
Andrei Kiselev <482003@mail.ru>
2018-05-30 23:22:39 +0300

    Changes

--- js/script.js
+++ js/script.js
@@ -1,16 +1,18 @@
 window.onload = function(){
 	var ques1 = prompt('Ваш бюджет на месяц?');
 	var ques2 = prompt('Название вашего магазина?');
-	var mainList = {
-		budget: ques1,
-		storeName: ques2
-	};
-	var shopgoods = [];
-	var employers = {
-		open: ''
-	};
 	var goodsType1 = prompt('Какой тип товаров будем продавать?');
 	var goodsType2 = prompt('Какой тип товаров будем продавать?');
 	var goodsType3 = prompt('Какой тип товаров будем продавать?');
+	var mainList = {
+		budget: ques1,
+		storeName: ques2,
+		shopGoods: [goodsType1, goodsType2, goodsType3],
+		employers: {},
+		open: false
+	};
+	
+	
+	
 	console.log(parseInt(ques1/30));
 }
